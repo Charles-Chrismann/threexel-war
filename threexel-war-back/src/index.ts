e558fff5e6d6d49c8a716f43e020fecee82a2d6d
Charles <charles.chrismann@free.fr>
2024-05-14 14:29:09 +0200

    add ws

--- threexel-war-back/src/index.ts
+++ threexel-war-back/src/index.ts
@@ -2,10 +2,19 @@ import 'dotenv/config'
 import { PrismaClient } from "@prisma/client";
 import express, { Request, Response } from "express";
 import * as http from 'http'
-import { Server } from 'socket.io'
+import { Server, Socket } from 'socket.io'
 import { compare, hash } from 'bcrypt'
-import { sign } from 'jsonwebtoken'
+import { sign, verify } from 'jsonwebtoken'
 import { PrismaClientKnownRequestError, PrismaClientValidationError } from "@prisma/client/runtime/library";
+
+declare module 'socket.io' {
+  export interface Socket {
+    user: {
+      id: number;
+      username: string;
+    };
+  }
+}
 
 const prisma = new PrismaClient()
 
@@ -51,13 +60,25 @@ app.post('/api/login', async (req: Request, res: Response) => {
   })
   console.log(user)
   if(!user) return res.sendStatus(401)
-  if(await compare(password, user.password)) return res.sendStatus(401)
+  if(!await compare(password, user.password)) return res.sendStatus(401)
   const token = sign({ id: user.id, username: user.username }, process.env.JWT_SECRET!)
   res.send(token)
 })
 
+io.use((socket, next) => {
+  const token = socket.handshake.auth.token
+  if(!token) return next()
+    try {
+      const payload = verify(token, process.env.JWT_SECRET!) as { id: number, username: string }
+      socket.user = payload
+      next();
+    } catch (error: unknown) {
+      
+    }
+});
+
 io.on('connection', (socket) => {
-  console.log('user connecyted')
+  console.log('user connecyted', socket.handshake.auth.token)
   socket.emit('message', {hello: "les jeunes"})
 
   socket.on('join room', async (roomId: string, userId: number) => {
@@ -80,6 +101,45 @@ io.on('connection', (socket) => {
       }
     })
     socket.emit('join room', roomData)
+  })
+
+  socket.on('place', async (voxel: { x: number, y: number, z: number, color: string, roomName: string }) => {
+    if(!socket.user) return
+    const txResult = await prisma.$transaction(async (tx) => {
+      const map = await tx.map.findFirst({
+        where: {
+          user: {
+            username: voxel.roomName
+          }
+        }
+      })
+      if(!map) return
+      const createdVoxel = tx.voxel.create({
+        data: {
+          x: voxel.x,
+          y: voxel.y,
+          z: voxel.z,
+          color: voxel.color,
+          mapId: map.id,
+          userId: socket.user.id
+        }
+      })
+      tx.userOnMap.update({
+        where: {
+          mapId_userId: {
+            mapId: map.id,
+            userId: socket.user.id
+          }
+        },
+        data: {
+          lastPlaced: new Date()
+        }
+      })
+      return true
+    })
+    if(!txResult) return
+    console.log(voxel)
+    io.emit('update voxel', voxel)
   })
 })
 
